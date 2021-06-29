18e88ea087b2661d95b94b06ccad9d561d83ac3f
shashidharks007 <71253211+shashidharks007@users.noreply.github.com>
2021-06-29 10:49:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App(){
+  const Searchbar = () => {
+    console.log("Search");
+  };
+
+  return(
+    <div>
+      <h1> hello world </h1>
+      <button> ________={Searchbar}Search </button>
+    </div>
+  );
+}
+
+export default App;
